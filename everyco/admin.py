43327b374c1dd5c847bd8aabc6c339aac87b11f6
shiblu <shiblu.msi@gmail.com>
2024-01-17 19:08:54 +0600

    Created model

--- everyco/admin.py
+++ everyco/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProductCategory
 
-# Register your models here.
+class ProductCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['uid','slug','name','parent_category','description','image']
+    #list_filter = ['is_vendor', 'is_customer']
+    
+admin.site.register(ProductCategory, ProductCategoryModelAdmin)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import VendorProfile
 
-# Register your models here.
+class VendorProfileModelAdmin(admin.ModelAdmin):
+    list_display = ['vendor_user','vendor_name','vendor_category','vendor_status']
+    #list_filter = ['is_vendor', 'is_customer']
+    
+admin.site.register(VendorProfile, VendorProfileModelAdmin)
